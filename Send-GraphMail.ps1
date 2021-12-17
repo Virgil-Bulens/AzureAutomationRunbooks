a24b085d65d4ce79b2b56ffd10d0544664cc458a
BULENS Virgil <github@anonymousdaddy.anonaddy.com>
2021-12-17 11:47:39 +0100

    replace invalid characters

--- Send-GraphMail.ps1
+++ Send-GraphMail.ps1
@@ -4,22 +4,23 @@
   Sends a simple mail with subject and content to a single recipient
 
 .DESCRIPTION
+
   Requires the following:
     - An App Registration with the Microsoft Graph API Mail.Send permission
       The mailboxes this app has this permission on, should be restricted using the Exchange Online "New-ApplicationAccessPolicy" cmdlet
     - A licensed Exchange Online user or shared mailbox
-    - An active Client Secret for the App Registration
-    - A Key Vault containing the Client Secret
-    - A System Assigned Managed Identity for the Automation Account this Runbook is in, with following roles:
-      - "Key Vault Secrets User" on the Key Vault Secret 
-      - "Key Vault Reader" on the Key Vault
+    - An active client secret for the App Registration
+    - A Key Vault containing the client secret
+    - A system assigned managed identity for the Automation Account this runbook is in
+      This identity should have at least the "Key Vault Secrets User" role on the client secret stored in the Key Vault, and the "Key Vault Reader" role on the Key Vault itself
+    - Following variables stored in the Automation Account:
+      - KeyVaultName
+      - SecretName
+      - TransactionalMailAppId
+      - From
+      - ExpirationWarningAddress
+    - Az.Accounts and Az.KeyVault modules imported in the Automation Account
 
-    - Automation Variables:
-      - "KeyVaultName" - Name of the Key Vault where the Client Secret is stored
-      - "SecretName" - Name of the Key Vault Secret
-      - "TransactionalMailAppId"
-      - "From" - Mail address of the mailbox
-      - "ExpirationWarningAddress" - Mail address to send warning to if the Client Secret is about to expire
 
 .PARAMETER To
   String mail address the mail will be sent to
@@ -32,7 +33,7 @@
 
 .NOTES
         Author: Virgil Bulens
-        Last Updated: 12/02/2021
+        Last Updated: 12/17/2021
     Version 1.1
 
 #>
@@ -72,48 +73,53 @@ Param(
 # Variables
 #
 $ErrorActionPreference = "Stop"
-
 $AutomationVariables = @(
-    "KeyVaultName",
-    "SecretName",
-    "From",
-    "ExpirationWarningAddress",
-    "TransactionalMailAppId"
+  "KeyVaultName",
+  "SecretName",
+  "From",
+  "ExpirationWarningAddress",
+  "TransactionalMailAppId"
 )
-
 foreach ($Variable in $AutomationVariables)
 {
-    New-Variable -Name $Variable `
-        -Value ( Get-AutomationVariable -Name $Variable )
+  Set-Variable -Name $Variable `
+    -Value ( Get-AutomationVariable -Name $Variable )
 }
+
+$InvalidCharacters = @(
+  "\",  
+  "`""
+)
 
 
 #
 # Authentication
 #
 # Az
-Connect-AzAccount -Identity | Out-Null
+$ConnectAzAccount = Connect-AzAccount -Identity
+if ( $ConnectAzAccount.Environments )
+{
+    
+}
+
+else
+{
+  Write-Error -Message "Could not connect to Azure!"
+}
 
 
 #
 # Main
 #
 # Get required variables and secrets
-$TenantId = Get-AzContext | `
-    ForEach-Object Tenant | `
-    ForEach-Object Id
-$AppSecret = Get-AzKeyVaultSecret -VaultName $KeyVaultName `
-    -Name $SecretName `
-    -AsPlainText
+$TenantId = Get-AzContext | ForEach-Object Tenant | ForEach-Object Id
+$AppSecret = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $SecretName -AsPlainText
 
 # Check if key is about to expire
 $KeyExpiring = $false
-$KeyExpirationDate = Get-AzKeyVaultSecret -VaultName $KeyVaultName `
-    -Name $SecretName | `
-    ForEach-Object Expires
+$KeyExpirationDate = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $SecretName | ForEach-Object Expires
 $Today = Get-Date
-$DaysUntilExpiration = $KeyExpirationDate - $Today | `
-    ForEach-Object Days
+$DaysUntilExpiration = $KeyExpirationDate - $Today | ForEach-Object Days
 
 if ($DaysUntilExpiration -le 10)
 {
@@ -121,12 +127,9 @@ if ($DaysUntilExpiration -le 10)
 }
 
 # Get authentication token from App Registration "Transactional Mail"
-$Uri = "https://login.microsoftonline.com/" + `
-    $TenantId + `
-    "/oauth2/v2.0/token"
-
+$Uri = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
 $Body = @{
-  'client_id'     = $AppId
+  'client_id'     = $TransactionalMailAppId
   'scope'         = "https://graph.microsoft.com/.default"
   'client_secret' = $AppSecret
   'grant_type'    = "client_credentials"
@@ -141,6 +144,13 @@ $Parameters = @{
 
 $TokenRequest = Invoke-RestMethod @Parameters
 $Token = $TokenRequest.access_token
+
+# Replace invalid characters in Subject and Content
+foreach ( $Character in $InvalidCharacters )
+{
+  $Subject = $Subject.Replace($Character, "\$Character")
+  $Content = $Content.Replace($Character, "\$Character")
+}
 
 # Send mail using Graph API
 $Headers = @{
@@ -169,9 +179,7 @@ $MailMessage = @"
 "@
 
 $Parameters = @{
-  'URI'         = "https://graph.microsoft.com/v1.0/users/" + `
-    $From + `
-    "/sendMail"
+  'URI'         = "https://graph.microsoft.com/v1.0/users/$From/sendMail"
   'Headers'     = $Headers
   'Method'      = "Post"
   'ContentType' = "application/json"
@@ -188,7 +196,7 @@ if ($KeyExpiring)
   $ResourceGroupName = $KeyVault.ResourceGroupName
 
   $Subject = "App Secret for Transactional Mail expiring in $DaysUntilExpiration days."
-  $Content = "<p>The&nbsp;app&nbsp;secret&nbsp;for&nbsp;\`"Transactional&nbsp;Mail\`"&nbsp;will&nbsp;expire&nbsp;in&nbsp;$DaysUntilExpiration&nbsp;days.<br />Make&nbsp;sure&nbsp;to&nbsp;renew&nbsp;it&nbsp;here:&nbsp;<br />https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/Credentials/appId/$AppId/isMSAApp/</p><p>Also&nbsp;update&nbsp;the&nbsp;new&nbsp;value&nbsp;and&nbsp;expiration&nbsp;date&nbsp;in&nbsp;the&nbsp;key&nbsp;vault:&nbsp;<br />https://portal.azure.com/#blade/Microsoft_Azure_KeyVault/ListObjectVersionsRBACBlade/overview/objectType/secrets/objectId/https%3A%2F%2F$KeyVaultName.vault.azure.net%2Fsecrets%2F$SecretName/vaultResourceUri/%2Fsubscriptions%2F$SubscriptionId%2FresourceGroups%2F$ResourceGroupName%2Fproviders%2FMicrosoft.KeyVault%2Fvaults%2F$KeyVaultName/vaultId/%2Fsubscriptions%2F$SubscriptionId%2FresourceGroups%2F$ResourceGroupName%2Fproviders%2FMicrosoft.KeyVault%2Fvaults%2F$KeyVaultName</p>"
+  $Content = "<p>The&nbsp;app&nbsp;secret&nbsp;for&nbsp;\`"Transactional&nbsp;Mail\`"&nbsp;will&nbsp;expire&nbsp;in&nbsp;$DaysUntilExpiration&nbsp;days.<br />Make&nbsp;sure&nbsp;to&nbsp;renew&nbsp;it&nbsp;here:&nbsp;<br />https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/Credentials/appId/$TransactionalMailAppId/isMSAApp/</p><p>Also&nbsp;update&nbsp;the&nbsp;new&nbsp;value&nbsp;and&nbsp;expiration&nbsp;date&nbsp;in&nbsp;the&nbsp;key&nbsp;vault:&nbsp;<br />https://portal.azure.com/#blade/Microsoft_Azure_KeyVault/ListObjectVersionsRBACBlade/overview/objectType/secrets/objectId/https%3A%2F%2F$KeyVaultName.vault.azure.net%2Fsecrets%2F$SecretName/vaultResourceUri/%2Fsubscriptions%2F$SubscriptionId%2FresourceGroups%2F$ResourceGroupName%2Fproviders%2FMicrosoft.KeyVault%2Fvaults%2F$KeyVaultName/vaultId/%2Fsubscriptions%2F$SubscriptionId%2FresourceGroups%2F$ResourceGroupName%2Fproviders%2FMicrosoft.KeyVault%2Fvaults%2F$KeyVaultName</p>"
 
   $MailMessage = @"
 {
@@ -211,9 +219,7 @@ if ($KeyExpiring)
 "@
 
   $Parameters = @{
-    'URI'         = "https://graph.microsoft.com/v1.0/users/" + `
-        $From + `
-        "/sendMail"
+    'URI'         = "https://graph.microsoft.com/v1.0/users/$From/sendMail"
     'Headers'     = $Headers
     'Method'      = "Post"
     'ContentType' = "application/json"
